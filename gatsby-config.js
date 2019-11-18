4b90c70058f7cb4632364cfcf206562f9c1e8092
Tanvir Aslam Mohammed <teemo@amazon.com>
2019-11-18 00:20:47 -0800

    Setup with gatsby starter kit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `GT3 Documentation Portal`,
+    description: `GT3 Documentation Portal`,
+    author: "@teemo",
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
@@ -42,6 +42,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-plugin-netlify-cms`,
         ],
       },
     },
